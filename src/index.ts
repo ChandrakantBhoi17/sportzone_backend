332c27b98d39c6fcee9a954a646f2d9c6b14aea4
Chandrakant Bhoi <75374305+ChandrakantBhoi17@users.noreply.github.com>
2026-04-08 00:06:25 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 4000;
 
 // Middleware
 app.use(cors({ 
-  origin: ['http://localhost:5173', 'http://localhost:8080', 'http://127.0.0.1:5173', 'http://127.0.0.1:8080']
+  origin: ['http://localhost:5173', 'http://localhost:8080', 'http://127.0.0.1:5173', 'http://127.0.0.1:8080','https://truptienterprises.in]
 }));
 app.use(express.json());
 
